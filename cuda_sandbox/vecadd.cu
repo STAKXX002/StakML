1dd5156937af1347c8a4370f01a2500d64d87675
STAKXX002 <matrixmaximus007@gmail.com>
2026-06-25 19:18:22 +0530

    style: format error message for better readability in CUDA_CHECK macro

--- cuda_sandbox/vecadd.cu
+++ cuda_sandbox/vecadd.cu
@@ -4,7 +4,8 @@
 #define CUDA_CHECK(call) do { \
     cudaError_t err = call; \
     if (err != cudaSuccess) { \
-        fprintf(stderr, "CUDA error %s:%d: %s\n", __FILE__, __LINE__, cudaGetErrorString(err)); \
+        fprintf(stderr, "CUDA error %s:%d: %s\n", __FILE__, __LINE__, \
+                cudaGetErrorString(err)); \
         exit(1); \
     } \
 } while(0)
